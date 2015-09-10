37e8341e140fbb006b47c797937ba034ebf8bb90
heroxdream <heroxdream@gmail.com>
2015-09-10 14:20:17 -0400

    first commit

--- src/decisiontree/Tree.java
+++ src/decisiontree/Tree.java
@@ -1,7 +1,20 @@
 package decisiontree;
 
+
 /**
  * Created by hanxuan on 9/10/15.
  */
 public class Tree {
+
+    private Tree left;
+    private Tree right;
+
+    public Tree(){
+
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("hello java");
+    }
 }
