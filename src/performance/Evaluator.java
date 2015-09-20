59e94565eda758c5a2d792b38988c9c2a4bef50b
heroxdream <heroxdream@gmail.com>
2015-09-20 17:38:59 -0400

    Performance structure finished

--- src/performance/Evaluator.java
+++ src/performance/Evaluator.java
@@ -1,7 +1,57 @@
 package performance;
 
+import com.google.common.util.concurrent.AtomicDouble;
+import data.DataSet;
+import data.core.Label;
+import model.Predictable;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.IntStream;
+
 /**
  * Created by hanxuan on 9/17/15.
  */
 public class Evaluator {
+
+    private static final Logger log = LogManager.getLogger(Evaluator.class);
+
+    protected DataSet testSet = null;
+
+    protected Label predictLabel = null;
+
+    public Evaluator() {}
+
+    public Evaluator(DataSet data) {
+
+        testSet = data;
+    }
+
+    public void getPredictLabel(Predictable model) {
+
+        int instanceLength = testSet.getInstanceLength();
+        float[] predict = new float[instanceLength];
+        IntStream.range(0, instanceLength).forEach(
+                i -> predict[i] = (float) model.predict(testSet.getInstance(i))
+        );
+        predictLabel = new Label(predict, null);
+    }
+
+    public double evaluate() {
+
+        Label trueLabel = testSet.getLabels();
+        int instanceLength = testSet.getInstanceLength();
+        AtomicDouble accu = new AtomicDouble(0);
+        IntStream.range(0, instanceLength).forEach(
+                i -> accu.getAndAdd(Math.pow(trueLabel.getRow(i) - predictLabel.getRow(i), 2))
+        );
+        return accu.get() / (double) instanceLength;
+    }
+
+    public void setTestSet(DataSet testSet) {
+        this.testSet = testSet;
+    }
+
+
+
 }
