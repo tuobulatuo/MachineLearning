6b3c6e1e4c5fc2cf971558cee763b44578b8f8c0
heroxdream <heroxdream@gmail.com>
2015-10-02 20:07:30 -0400

    neural network finished

--- src/model/supervised/neuralnetwork/NNMain.java
+++ src/model/supervised/neuralnetwork/NNMain.java
@@ -3,11 +3,9 @@ package model.supervised.neuralnetwork;
 import data.DataSet;
 import data.builder.Builder;
 import data.builder.FullMatrixDataSetBuilder;
-import data.core.Norm;
-import model.supervised.linearmodel.LogisticGradientDecent;
-import performance.ClassificationEvaluator;
-import performance.CrossValidationEvaluator;
-import performance.Evaluator;
+
+import java.util.Arrays;
+
 
 /**
  * Created by hanxuan on 10/1/15 for machine_learning.
@@ -33,15 +31,49 @@ public class NNMain {
 
         DataSet dataset = builder.getDataSet();
         dataset.meanVarianceNorm();
+        Perceptron.ALPHA = 0.0025;
         Perceptron perceptron = new Perceptron();
         perceptron.initialize(dataset);
         perceptron.train();
 
     }
 
+
+    public static void neuralNetworkTest() throws Exception{
+
+        String path = "/Users/hanxuan/Dropbox/neu/fall15/machine learning/data/nn.data";
+        String sep = "\\s+";
+        boolean hasHeader = false;
+        boolean needBias = false;
+        int m = 8;
+        int n = 8;
+        int[] featureCategoryIndex = {};
+        boolean classification = false;
+
+        Builder builder =
+                new FullMatrixDataSetBuilder(path, sep, hasHeader, needBias, m, n, featureCategoryIndex, classification);
+
+        builder.build();
+
+        DataSet dataset = builder.getDataSet();
+
+        int[] structure = {8, 3, 8};
+        boolean biased = false;
+        MultilayerPerceptron.BUCKET_COUNT = 1;
+        MultilayerPerceptron nn = new MultilayerPerceptron(structure, biased);
+        nn.initialize(dataset);
+        nn.train();
+
+        for (int i = 0; i < dataset.getInstanceLength(); i++) {
+            double[] X = dataset.getInstance(i);
+            System.out.println(Arrays.toString(X) + " => " + Arrays.toString(nn.yVector((int)nn.predict(X))));
+        }
+    }
+
     public static void main(String[] args) throws Exception{
 
-        Perceptron.ALPHA = 0.0025;
-        perceptronTest();
+//        perceptronTest();
+
+        neuralNetworkTest();
     }
 }
