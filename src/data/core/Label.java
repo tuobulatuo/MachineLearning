651237409a54af8fdf224dc31b2c6edd8a02ac15
heroxdream <heroxdream@gmail.com>
2015-10-17 10:55:55 -0400

    add classIndex map and indexClass map

--- src/data/core/Label.java
+++ src/data/core/Label.java
@@ -1,8 +1,6 @@
 package data.core;
 
-import java.util.Arrays;
-import java.util.EnumMap;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.stream.IntStream;
 
 /**
@@ -12,25 +10,36 @@ public class Label {
 
     private float[] vector = null;
 
-    private HashSet<String> categories = null;
+    private HashMap<Integer, Integer> classIndexMap = null;
 
-    private boolean isCategory = false;
+    private HashMap<Integer, Integer> indexClassMap = null;
 
-//    public Label (float[] data) {
-//
-//        this.vector = data;
-//    }
+//    private boolean isCategory = false;
 
-    public Label(float[] data, HashSet<String> categories) {
+    public Label(float[] data, HashMap<Integer, Integer> categories) {
 
         this.vector = data;
-        this.categories = categories;
+        this.classIndexMap = categories;
+        if (classIndexMap != null) {
+            indexClassMap = new HashMap<>();
+            for (int k : classIndexMap.keySet()) {
+                indexClassMap.put(classIndexMap.get(k), k);
+            }
+        }
     }
 
-    public boolean isCategoryLabel() {
+    public double getLabelQuotient(int category) {
 
-        return isCategory;
+        float l = (float) (category * 1.0);
+        int counter = 0;
+        for (float e : vector) if (e == l) ++ counter;
+        return counter / (double) vector.length;
     }
+
+//    public boolean isCategoryLabel() {
+//
+//        return isCategory;
+//    }
 
     public double getRow(int rowNum) {
 
@@ -42,14 +51,18 @@ public class Label {
         return vector;
     }
 
-    public HashSet<String> getCategories() {
-        return categories;
+    public HashMap<Integer, Integer> getClassIndexMap() {
+        return classIndexMap;
     }
 
-    public Label subLableByRow (int[] subIndexes) {
+    public HashMap<Integer, Integer> getIndexClassMap() {
+        return indexClassMap;
+    }
+
+    public Label subLabelByRow(int[] subIndexes) {
         float[] suVector = new float[subIndexes.length];
         IntStream.range(0, subIndexes.length).forEach(i -> suVector[i] = vector[subIndexes[i]]);
-        return new Label(suVector, this.categories);
+        return new Label(suVector, this.classIndexMap);
     }
 
     public static void main(String[] args) {
