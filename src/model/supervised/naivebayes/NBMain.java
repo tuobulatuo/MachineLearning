6ab7394f873f433638ae14439703f5f35175774a
heroxdream <heroxdream@gmail.com>
2015-10-18 21:42:41 -0400

    mixGaussian for NB test failed

--- src/model/supervised/naivebayes/NBMain.java
+++ src/model/supervised/naivebayes/NBMain.java
@@ -1,5 +1,6 @@
 package model.supervised.naivebayes;
 
+import algorithms.parameterestimate.MixtureGaussianEM;
 import data.DataSet;
 import data.builder.Builder;
 import data.builder.FullMatrixDataSetBuilder;
@@ -73,15 +74,50 @@ public class NBMain {
 
     }
 
+    public static void mixGaussianNBTest() throws Exception{
+
+        String path = "/Users/hanxuan/Dropbox/neu/fall15/machine learning/data/spambase/spambase.data";;
+        String sep = ",";
+        boolean hasHeader = false;
+        boolean needBias = false;
+        int m = 57;
+        int n = 4700;
+        int[] featureCategoryIndex = {};
+        boolean classification = true;
+
+        Builder builder =
+                new FullMatrixDataSetBuilder(path, sep, hasHeader, needBias, m, n, featureCategoryIndex, classification);
+
+        builder.build();
+
+        DataSet dataset = builder.getDataSet();
+
+        MixtureGaussianEM.THRESHOLD = 0;
+
+        MixtureGaussian.COMPONENTS = 9;
+        MixtureGaussian.MAX_THREADS = 2;
+        MixtureGaussian mixtureGaussianNB = new MixtureGaussian();
+
+        ClassificationEvaluator.ROC = false;
+        ClassificationEvaluator.POS = 1;
+        ClassificationEvaluator.CONFUSION_MATRIX = false;
+        ClassificationEvaluator eva = new ClassificationEvaluator();
+        CrossValidationEvaluator crossEvaluator = new CrossValidationEvaluator(eva, dataset, 10, Norm.NULL);
+        crossEvaluator.crossValidateEvaluate(mixtureGaussianNB);
+
+    }
+
     public static void main(String[] args) throws Exception{
 
-        multinoulliNBTest(2, new double[]{80});
-        System.out.println("\n\n\n");
-        multinoulliNBTest(4, new double[]{25, 50, 75});
-        System.out.println("\n\n\n");
-        multinoulliNBTest(9, new double[]{11, 22, 33, 44, 55, 66, 77, 88});
+//        multinoulliNBTest(2, new double[]{80});
+//        System.out.println("\n\n\n");
+//        multinoulliNBTest(4, new double[]{25, 50, 75});
+//        System.out.println("\n\n\n");
+//        multinoulliNBTest(9, new double[]{11, 22, 33, 44, 55, 66, 77, 88});
+//
+//        gaussianNBTest();
 
-        gaussianNBTest();
 
+        mixGaussianNBTest();
     }
 }
