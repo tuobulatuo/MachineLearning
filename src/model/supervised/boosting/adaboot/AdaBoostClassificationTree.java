f465a0099599ed68709fbad647a257155c5d9898
heroxdream <heroxdream@gmail.com>
2015-11-01 16:49:59 -0500

    SAMME by sampling from data points test passed

---
+++ src/model/supervised/boosting/adaboot/AdaBoostClassificationTree.java
@@ -0,0 +1,53 @@
+package model.supervised.boosting.adaboot;
+
+import data.DataSet;
+import gnu.trove.map.hash.TDoubleDoubleHashMap;
+import model.supervised.cart.ClassificationTree;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.neu.util.array.ArraySumUtil;
+import org.neu.util.rand.RandomUtils;
+import org.neu.util.sort.SortDoubleDoubleUtils;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * Created by hanxuan on 10/30/15 for machine_learning.
+ */
+public class AdaBoostClassificationTree extends ClassificationTree implements AdaBoostClassifier {
+
+    private static Logger log = LogManager.getLogger(AdaBoostClassificationTree.class);
+
+    public AdaBoostClassificationTree(){}
+
+    public AdaBoostClassificationTree(int depth, DataSet dataSet, int[] existIds){
+        super(depth, dataSet, existIds);
+    }
+
+    @Override
+    protected void split(int[] leftGroup, int[] rightGroup) {
+
+        left = new AdaBoostClassificationTree(this.depth + 1, this.dataSet, leftGroup);
+        right = new AdaBoostClassificationTree(this.depth + 1, this.dataSet, rightGroup);
+    }
+
+    @Override
+    public void boostInitialize(DataSet data, double[] weights) {
+
+        this.dataSet = data;
+        this.existIds = RandomUtils.getIndexes(data.getInstanceLength());
+
+        log.debug("BoostInitialize finished, Tree WEIGHTED randomness: {}", randomness);
+    }
+
+    @Override
+    public void boost() {
+        train();
+    }
+
+    @Override
+    public double boostPredict(double[] feature) {
+        return predict(feature);
+    }
+}
---
+++ src/model/supervised/boosting/adaboot/SAMMESampleSimulation.java
@@ -0,0 +1,65 @@
+package model.supervised.boosting.adaboot;
+
+import data.DataSet;
+import org.apache.commons.math3.distribution.EnumeratedIntegerDistribution;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.neu.util.array.ArraySumUtil;
+import org.neu.util.rand.RandomUtils;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by hanxuan on 11/1/15 for machine_learning.
+ */
+public class SAMMESampleSimulation extends SAMME{
+
+    public static int SAMPLE_SIZE_COEF = 1;
+
+    private static Logger log = LogManager.getLogger(SAMMESampleSimulation.class);
+
+    private int[] indexes = null;
+
+    @Override
+    public void train() {
+
+        indexes = RandomUtils.getIndexes(weights.length);
+
+        for (int i = 0; i < adaBoostClassifiers.length; i++) {
+
+            AdaBoostClassifier classifier = adaBoostClassifiers[i];
+
+            EnumeratedIntegerDistribution integerDistribution = new EnumeratedIntegerDistribution(indexes, weights);
+            int[] trainingIndexes = integerDistribution.sample(indexes.length * SAMPLE_SIZE_COEF);
+            DataSet roundTrainData = trainingData.subDataSetByRow(trainingIndexes);
+            classifier.boostInitialize(roundTrainData, weights);
+            classifier.boost();
+
+            double error = getWeightedError(classifier);
+            roundError[i] = error;
+
+            alpha[i] = Math.log((1 - error) / error) / 2 + Math.log(classCount - 1);
+
+            modifyWeights(classifier, alpha[i]);
+            ArraySumUtil.normalize(weights);
+
+            if (NEED_ROUND_REPORT) {
+                statisticReport(i, error);
+            }
+
+            log.info("{} round boosting finished ...", i);
+        }
+
+        log.info("SAMMESampleSimulation Training finished ...");
+
+        if (NEED_ROUND_REPORT) {
+            printRoundReport();
+        }
+
+        try{
+            TimeUnit.SECONDS.sleep(1);
+        }catch (Exception e){
+            log.error(e.getMessage(), e);
+        }
+    }
+}
