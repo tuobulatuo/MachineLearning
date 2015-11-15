6807f914700c20de98a34bdbff1cc6d8b6c64ae3
heroxdream <heroxdream@gmail.com>
2015-11-14 20:05:37 -0500

    minor change

--- src/feature/pca/PCAMain.java
+++ src/feature/pca/PCAMain.java
@@ -3,10 +3,8 @@ package feature.pca;
 import data.DataSet;
 import data.builder.Builder;
 import data.builder.FullMatrixDataSetBuilder;
-import gnu.trove.set.hash.TIntHashSet;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import performance.CrossValidationEvaluator;
 import utils.random.RandomUtils;
 
 import java.io.BufferedWriter;
@@ -24,34 +22,29 @@ public class PCAMain {
 
     public static void main(String[] args) throws Exception{
 
-        String path = "/Users/hanxuan/Dropbox/neu/fall15/data mining/project/data/clean/data.all.txt";
+        String path = "/Users/hanxuan/Dropbox/neu/fall15/machine learning/data/spam_polluted/allSet";
         String sep = "\t";
         boolean hasHeader = false;
         boolean needBias = false;
-        int m = 51;
-        int n = 870000;
-        int[] featureCategoryIndex = {0, 1, 2, 3, 4, 5, 6, 7};
-        boolean isClassification = true;
+        int m = 1057;
+        int n = 4601;
+        int[] featureCategoryIndex = {};
+        boolean classification = true;
 
         Builder builder =
-                new FullMatrixDataSetBuilder(path, sep, hasHeader, needBias, m, n, featureCategoryIndex, isClassification);
+                new FullMatrixDataSetBuilder(path, sep, hasHeader, needBias, m, n, featureCategoryIndex, classification);
 
         builder.build();
 
         DataSet dataset = builder.getDataSet();
         dataset.meanVarianceNorm();
 
-        int[][] kFoldIndex = CrossValidationEvaluator.partition(dataset, 100);
-        TIntHashSet trainIndexes = new TIntHashSet(kFoldIndex[0]);
-        DataSet trainSet = dataset.subDataSetByRow(trainIndexes.toArray());
-        trainSet.meanVarianceNorm();
-
-        PCA pca = new PCA(trainSet);
+        PCA pca = new PCA(dataset);
         pca.rotate();
 
-        double totalVar = pca.totalVar(trainSet);
-        double[] pve = new double[trainSet.getFeatureLength()];
-        IntStream.range(0, pve.length).forEach(j -> pve[j] = pca.varianceExplainedM(j, trainSet) / totalVar);
+        double totalVar = pca.totalVar(dataset);
+        double[] pve = new double[dataset.getFeatureLength()];
+        IntStream.range(0, pve.length).forEach(j -> pve[j] = pca.varianceExplainedM(j, dataset) / totalVar);
         double[] cpve = new double[pve.length];
         cpve[0] = pve[0];
         for (int j = 1; j < cpve.length; j++) {
@@ -65,20 +58,8 @@ public class PCAMain {
         log.info("{}", cpve);
         log.info("==============");
 
-
-        double totalVar2 = pca.totalVar(dataset);
-        IntStream.range(0, pve.length).parallel().forEach(j -> pve[j] = pca.varianceExplainedM(j, dataset) / totalVar2);
-        cpve[0] = pve[0];
-        for (int j = 1; j < cpve.length; j++) {
-            cpve[j] = cpve[j - 1] + pve[j];
-        }
-
-        log.info("{}", pve);
-        log.info("{}", cpve);
-        log.info("\n");
-
-        DataSet principleTopNSet = pca.project(dataset, 50);
-        String pcaOut = "/Users/hanxuan/Dropbox/neu/fall15/data mining/project/data/clean/data.all.pca.txt";
+        DataSet principleTopNSet = pca.project(dataset, 10);
+        String pcaOut = "/Users/hanxuan/Dropbox/neu/fall15/machine learning/data/spam_polluted/allSet.pca.10";
         BufferedWriter writer = new BufferedWriter(new FileWriter(pcaOut), 1024 * 1024 * 32);
         Map indexClassMap = dataset.getLabels().getIndexClassMap();
         for (int i = 0; i < principleTopNSet.getInstanceLength(); i++) {
