f22621cf9f80cf3404357ff0b1f99684a4a5e60d
heroxdream <heroxdream@gmail.com>
2015-11-16 16:12:27 -0500

    add probs method

--- src/model/Predictable.java
+++ src/model/Predictable.java
@@ -4,8 +4,17 @@ package model;
  * Created by hanxuan on 9/18/15 for machine_learning.
  */
 public interface Predictable {
+
     double predict(double[] feature);
+
     default double score(double[] feature){
         return predict(feature);
     }
+
+    default double[] probs(double[] feature){
+        System.out.println("default probs method not allowed, exit");
+        System.exit(-1);
+        return new double[0];
+    }
+
 }
