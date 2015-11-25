48554451b470af270767a9b419bb8e4f59da8939
heroxdream <heroxdream@gmail.com>
2015-11-25 14:28:15 -0500

    add euclidean distance and inner product

--- src/utils/array/ArrayUtil.java
+++ src/utils/array/ArrayUtil.java
@@ -1,7 +1,14 @@
 package utils.array;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+
 public class ArrayUtil {
+
+	private static Logger log = LogManager.getLogger(ArrayUtil.class);
 
 	public static void reverse(int[] array) {
 		for (int i = 0; i < array.length / 2; i++) {
@@ -55,4 +62,33 @@ public class ArrayUtil {
 //		else
 //			return str;
 //	}
+
+	public static double innerProduct(double[] x1, double[] x2) {
+
+		double result = 0;
+		for (int i = 0; i < x1.length; i++) {
+			result += x1[i] * x2[i];
+		}
+
+		return result;
+	}
+
+	public static double[] arraySubtract(double[] x1, double[] x2) {
+
+		double[] result = new double[x1.length];
+		for (int i = 0; i < x1.length; i++) {
+			result[i] = x1[i] - x2[i];
+		}
+
+		return result;
+	}
+
+	public static double euclidean(double[] x1, double[] x2) {
+		double[] x3 = arraySubtract(x1, x2);
+		return Arrays.stream(x3).map(xi -> Math.pow(xi, 2)).sum();
+	}
+
+	public static double normL2(double[] x) {
+		return Math.pow(Arrays.stream(x).map(xi -> Math.pow(xi, 2)).sum(), 0.5);
+	}
 }
