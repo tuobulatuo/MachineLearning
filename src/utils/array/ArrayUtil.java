37d67d5e806b26f18f6ce81963cd562d62585abe
heroxdream <heroxdream@gmail.com>
2015-11-28 12:45:07 -0500

    removed unused methods

--- src/utils/array/ArrayUtil.java
+++ src/utils/array/ArrayUtil.java
@@ -17,22 +17,6 @@ public class ArrayUtil {
 			array[array.length - i - 1] = temp;
 		}
 	}
-
-//	public static void reverse(long[] array) {
-//		for (int i = 0; i < array.length / 2; i++) {
-//			long temp = array[i];
-//			array[i] = array[array.length - i - 1];
-//			array[array.length - i - 1] = temp;
-//		}
-//	}
-//
-//	public static void reverse(float[] array) {
-//		for (int i = 0; i < array.length / 2; i++) {
-//			float temp = array[i];
-//			array[i] = array[array.length - i - 1];
-//			array[array.length - i - 1] = temp;
-//		}
-//	}
     
 	public static void reverse(double[] array) {
 		for (int i = 0; i < array.length / 2; i++) {
@@ -41,27 +25,6 @@ public class ArrayUtil {
 			array[array.length - i - 1] = temp;
 		}
 	}
-
-//	public static String fill(String str, int length) {
-//
-//		String template = "";
-//		for (int i = 0; i < length; i++) {
-//			template += " ";
-//		}
-//
-//		int count = 0;
-//		for (char c : str.toCharArray()) {
-//			if (c < 127) {
-//				count++;
-//			} else {
-//				count += 2;
-//			}
-//		}
-//		if (count < length)
-//			return str + template.substring(count);
-//		else
-//			return str;
-//	}
 
 	public static double innerProduct(double[] x1, double[] x2) {
 
@@ -80,7 +43,6 @@ public class ArrayUtil {
 		for (int i = 0; i < x1.length; i++) {
 			result[i] = x1[i] - x2[i];
 		}
-
 		return result;
 	}
 
@@ -89,9 +51,5 @@ public class ArrayUtil {
 		double result = 0;
 		for (double x: x3) if (x != 0) result += x * x;
 		return result;
-	}
-
-	public static double normL2(double[] x) {
-		return Math.pow(Arrays.stream(x).map(xi -> Math.pow(xi, 2)).sum(), 0.5);
 	}
 }
