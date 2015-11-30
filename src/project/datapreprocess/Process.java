4ab71a7c658ff12645d8482998c51b084fa456d9
heroxdream <heroxdream@gmail.com>
2015-11-30 09:43:58 -0500

    remove unnecessary methods

--- src/project/datapreprocess/Process.java
+++ src/project/datapreprocess/Process.java
@@ -1,5 +1,6 @@
 package project.datapreprocess;
 
+import gnu.trove.map.hash.TObjectIntHashMap;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import utils.NumericalComputation;
@@ -9,8 +10,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.*;
 import java.util.stream.IntStream;
 
 /**
@@ -56,42 +56,65 @@ public abstract class Process {
         log.info("{} outlier correct", outlier);
     }
 
-    public void featureExpand(String in1, String in2, String out) throws Exception{
+    public void mark(String in, String output) throws Exception{
+
+        String line;
+        BufferedReader reader = new BufferedReader(new FileReader(in), 1024 * 1024 * 64);
+        TObjectIntHashMap<String> counter = new TObjectIntHashMap<>(180000);
+        while ((line = reader.readLine()) != null) {
+            String[] es = line.split("\t");
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < es.length - 1; i++) sb.append(es[i] + "\t");
+
+            counter.adjustOrPutValue(sb.toString(), 1, 1);
+        }
+        reader.close();
+
+        reader = new BufferedReader(new FileReader(in), 1024 * 1024 * 64);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(output), 1024 * 1024 * 64);
+        while ((line = reader.readLine()) != null) {
+            String[] es = line.split("\t");
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < es.length - 1; i++) sb.append(es[i] + "\t");
+
+            String entry = sb.toString();
+
+            sb.append((counter.get(entry) > 1 ? 1 : 0) + "\t");
+            sb.append(es[es.length - 1]);
+            writer.write(sb.toString() + "\n");
+        }
+        reader.close();
+        writer.close();
+
+        log.info("mark ..");
+        log.info("{} duplicate feature", counter.size());
+    }
+
+    public void featureExpand(String in1, String out) throws Exception{
 
         HashSet<String> classes = new HashSet<>();
         HashSet<String> addresses = new HashSet<>();
-        HashSet<String> resos = new HashSet<>();
-        HashSet<String> descs = new HashSet<>();
+
         uniqClassAddress(in1, classes, addresses);
-        uniqResoDesc(in2, resos, descs);
 
         String[] classesArray = classes.toArray(new String[0]);
         String[] addressArray = addresses.toArray(new String[0]);
-        String[] resosArray = resos.toArray(new String[0]);
-        String[] descsArray = descs.toArray(new String[0]);
 
         Arrays.sort(classesArray);
         Arrays.sort(addressArray);
-        Arrays.sort(resosArray);
-        Arrays.sort(descsArray);
 
 
         float[][] tableClass = new float[addressArray.length][classesArray.length];
-        float[][] tableReso = new float[addressArray.length][resosArray.length];
-        float[][] tableDesc = new float[addressArray.length][descsArray.length];
         IntStream.range(0, tableClass.length).forEach(i -> Arrays.fill(tableClass[i], 1)); // add one laplace smoothing
-        IntStream.range(0, tableReso.length).forEach(i -> Arrays.fill(tableReso[i], 1)); // add one laplace smoothing
-        IntStream.range(0, tableDesc.length).forEach(i -> Arrays.fill(tableDesc[i], 1)); // add one laplace smoothing
 
-        countClass(in1, tableClass, classesArray, addressArray, 878049);
-        countDescriptionResolution(in2, addressArray, resosArray, descsArray, tableReso, tableDesc);
+        count(in1, tableClass, classesArray, addressArray, 878049);
 
         double[] logOddsAddress = new double[addressArray.length];
         logOddsAddress(in1, addressArray, logOddsAddress);
 
         tableProcess(tableClass);
-        tableProcess(tableReso);
-        tableProcess(tableDesc);
 
         write(in1, out, tableClass, addressArray, logOddsAddress);
     }
@@ -101,7 +124,6 @@ public abstract class Process {
         BufferedReader reader = new BufferedReader(new FileReader(path), 1024 * 1024 * 64);
         String line;
         while ((line = reader.readLine()) != null) {
-
             String[] es = line.trim().split("\t");
             int addressIndex = Arrays.binarySearch(addressArray, es[3].trim());
             logOddsAddress[addressIndex] ++;
@@ -113,33 +135,27 @@ public abstract class Process {
         log.info("logOdds Address");
     }
 
-    private void uniqClassAddress(String path, HashSet<String> classes, HashSet<String> addresses) throws Exception{
+    private int uniqClassAddress(String path, HashSet<String> classes, HashSet<String> addresses) throws Exception{
 
         BufferedReader reader = new BufferedReader(new FileReader(path), 1024 * 1024 * 32);
         String line;
+        int lineCounter = 0;
         while ((line = reader.readLine()) != null) {
             String[] es = line.trim().split("\t");
             classes.add(es[es.length - 1].trim()); // class
             addresses.add(es[3].trim());
+            lineCounter++;
         }
-        log.info("classes countClass: {}", classes.size());
-        log.info("addresses countClass: {}", addresses.size());
+        reader.close();
+
+        log.info("classes count: {}", classes.size());
+        log.info("addresses count: {}", addresses.size());
+        log.info("total instance: {}", lineCounter);
+
+        return lineCounter;
     }
 
-    private void uniqResoDesc(String path, HashSet<String> resos, HashSet<String> descs) throws Exception{
-
-        BufferedReader reader = new BufferedReader(new FileReader(path), 1024 * 1024 * 32);
-        String line;
-        while ((line = reader.readLine()) != null) {
-            String[] es = line.trim().split("\t");
-            resos.add(es[1].trim()); // class
-            descs.add(es[2].trim());
-        }
-        log.info("resos countClass: {}", resos.size());
-        log.info("descs countClass: {}", descs.size());
-    }
-
-    private void countClass(String path, float[][] table, String[] classes, String[] addresses, int lineMax) throws Exception{
+    private void count(String path, float[][] table, String[] classes, String[] addresses, int lineMax) throws Exception{
 
         BufferedReader reader = new BufferedReader(new FileReader(path), 1024 * 1024 * 32);
         String line;
@@ -155,30 +171,10 @@ public abstract class Process {
 
             lineCounter ++;
         }
-        log.info("countClass table...");
+        log.info("count table...");
     }
 
-    private void countDescriptionResolution(String path, String[] addresses, String[] resos, String[] descs,
-                                            float[][] resoTable, float[][] descTable) throws Exception{
-        BufferedReader reader = new BufferedReader(new FileReader(path), 1024 * 1024 * 32);
-        String line;
-        while ((line = reader.readLine()) != null) {
-
-            String[] es = line.trim().split("\t");
-            String address = es[0].trim();
-            String reso = es[1].trim();
-            String desc = es[2].trim();
-            int addressIndex = Arrays.binarySearch(addresses, address);
-            int resoIndex = Arrays.binarySearch(resos, reso);
-            int descIndex = Arrays.binarySearch(descs, desc);
-
-            resoTable[addressIndex][resoIndex] ++;
-            descTable[addressIndex][descIndex] ++;
-        }
-        log.info("countClass table...");
-    }
-
-    private void write(String pathIn, String pathOut, float[][] table, String[] addresses, double[] logOddsAddress)
+    private void write(String pathIn, String pathOut, float[][] tableClass, String[] addresses, double[] logOddsAddress)
             throws Exception{
 
         BufferedReader reader = new BufferedReader(new FileReader(pathIn), 1024 * 1024 * 32);
@@ -191,12 +187,11 @@ public abstract class Process {
 
             String timeString = time(es[0]);
             StringBuilder builder = new StringBuilder(timeString);
-
             builder.append("\t");
             for (int i = 1; i < es.length; i++) {
 
                 if (i == 3) { // handle address string
-                    String addressString = address(es[i].trim(), addresses, table, logOddsAddress);
+                    String addressString = address(es[i].trim(), addresses, tableClass, logOddsAddress);
                     builder.append(addressString + "\t");
                 }else {
                     builder.append(es[i] + "\t");
@@ -248,7 +243,7 @@ public abstract class Process {
         }
     }
 
-    private String address(String address, String[] addresses, float[][] table, double[] logOddsAddress) {
+    private String address(String address, String[] addresses, float[][] tableClass, double[] logOddsAddress) {
 
         StringBuilder builder = new StringBuilder();
 
@@ -261,8 +256,8 @@ public abstract class Process {
         int addressIndex = Arrays.binarySearch(addresses, address);
         double logOdd = logOddsAddress[addressIndex];
         builder.append(logOdd + "\t");
-        float[] logOdds = table[addressIndex];
-        for (int i = 0; i < logOdds.length; i++) builder.append(logOdds[i] + "\t");
+
+        for (int i = 0; i < tableClass[addressIndex].length; i++) builder.append(tableClass[addressIndex][i] + "\t");
 
         return builder.toString().trim();
     }
