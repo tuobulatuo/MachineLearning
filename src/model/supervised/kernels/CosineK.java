ebb39ec87b231ca9a732265aa0a8de86809c2549
heroxdream <heroxdream@gmail.com>
2015-12-10 15:44:24 -0500

    rename

--- src/model/supervised/kernels/CosineK.java
+++ src/model/supervised/kernels/CosineK.java
@@ -5,7 +5,7 @@ import utils.array.ArrayUtil;
 /**
  * Created by hanxuan on 12/10/15 for machine_learning.
  */
-public class Cosine implements Kernel {
+public class CosineK implements Kernel {
     @Override
     public double similarity(double[] x1, double[] x2) {
         return ArrayUtil.innerProduct(x1, x2) / ArrayUtil.norm2(x1) / ArrayUtil.norm2(x2);
--- src/model/supervised/kernels/EuclideanK.java
+++ src/model/supervised/kernels/EuclideanK.java
@@ -5,7 +5,7 @@ import utils.array.ArrayUtil;
 /**
  * Created by hanxuan on 12/10/15 for machine_learning.
  */
-public class Euclidean implements Kernel{
+public class EuclideanK implements Kernel{
     @Override
     public double similarity(double[] x1, double[] x2) {
         return - ArrayUtil.euclidean(x1, x2);
